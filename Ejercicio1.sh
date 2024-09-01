b729eca1821c0c1c1d923e8c5abf509683895d63
HLussiatti <hernanlussiatti@gmail.com>
2024-09-01 12:09:21 -0300

    01/09/2024 - Ejercicio Integrador

--- Ejercicio1.sh
+++ Ejercicio1.sh
@@ -4,11 +4,11 @@
 sudo docker cp Datasets/. namenode:/home/Datasets
 
 # Ejecutar comandos dentro del contenedor "namenode"
-sudo docker exec -it namenode bash -c 
+sudo docker exec -it namenode bash
   
 # Crear una carpeta en HDFS
-hdfs dfs -mkdir -p /user/hadoop/data
+hdfs dfs -mkdir -p /data
 
 # Mover archivos .csv desde /home/Datasets a la carpeta HDFS /user/hadoop/data
-hdfs dfs -put /home/Datasets/*.csv /user/hadoop/data/
+hdfs dfs -put /home/Datasets/*.csv /data/
 echo "Archivos .csv copiados a HDFS en /user/hadoop/data/"
