32240b1a769806060951e1f061aa8feff0231f89
HLussiatti <hernanlussiatti@gmail.com>
2024-09-01 11:51:05 -0300

    01/09/2024 - Ejercicio Integrador

---
+++ Ejercicio1.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Copiar archivos desde el host al contenedor "namenode"
+sudo docker cp DS-M4-Herramientas_Big_Data/Datasets/. namenode:/home/Datasets
+
+# Ejecutar comandos dentro del contenedor "namenode"
+sudo docker exec -it namenode bash -c 
+  
+# Crear una carpeta en HDFS
+hdfs dfs -mkdir -p /user/hadoop/data
+
+# Mover archivos .csv desde /home/Datasets a la carpeta HDFS /user/hadoop/data
+hdfs dfs -put /home/Datasets/*.csv /user/hadoop/data/
+echo "Archivos .csv copiados a HDFS en /user/hadoop/data/"
---
+++ SOLUCIÓN.md
@@ -0,0 +1,14 @@
+# PASOS
+1. Hice un Fork del repositorio.
+2. Bajé el repositorio en mi máquina y en la máquina virtual.
+``` git clone https://github.com/HLussiatti/DS-M4-Herramientas_Big_Data.git ```
+3. Me ubico dentro de la carpeta creada.
+``` cd DS-M4-Herramientas_Big_Data ```
+
+
+# EJERCICIO 1:
+Dento de la carpeta busco el listado de archivos:
+``` sudo docker ls -la ```
+Ejecuto el contendor de la versión 1 que contiene solo Hadoop para el ejercicio 1.
+``` sudo docker-compose -f docker-compose-v1.yml up -d ```
+
