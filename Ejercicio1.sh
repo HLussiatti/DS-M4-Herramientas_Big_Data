978ecc9b8b4c528cc281e80ae3f9c396aded78c2
HLussiatti <hernanlussiatti@gmail.com>
2024-09-01 17:33:57 -0300

    01/09/2024 - Ejercicio Integrador

--- Ejercicio1.sh
+++ Ejercicio1.sh
@@ -2,25 +2,23 @@
 
 # Copiar archivos desde el host al contenedor "namenode"
 
-
-sudo docker cp Datasets/calendario/Calendario.csv namenode:/home/Datasets/calendario/Calendario.csv
-sudo docker cp Datasets/canaldeventa/CanalDeVenta.csv namenode:/home/Datasets/canaldeventa/CanalDeVenta.csv
-sudo docker cp Datasets/cliente/Cliente.csv namenode:/home/Datasets/cliente/Cliente.csv
-sudo docker cp Datasets/compra/Compra.csv namenode:/home/Datasets/compra/Compra.csv
-sudo docker cp Datasets/empleado/Empleado.csv namenode:/home/Datasets/empleado/Empleado.csv
-sudo docker cp Datasets/gasto/Gasto.csv namenode:/home/Datasets/gasto/Gasto.csv
-sudo docker cp Datasets/producto/Producto.csv namenode:/home/Datasets/producto/Producto.csv
-sudo docker cp Datasets/proveedor/Proveedor.csv namenode:/home/Datasets/proveedor/Proveedor.csv
-sudo docker cp Datasets/sucursal/Sucursal.csv namenode:/home/Datasets/sucursal/Sucursal.csv
-
-sudo docker cp Datasets/tipodegasto/TiposDeGasto.csv namenode:/home/Datasets/tipodegasto/TiposDeGasto.csv
-sudo docker cp Datasets/venta/Venta.csv namenode:/home/Datasets/venta/Venta.csv
+sudo docker cp Datasets/calendario/Calendario.csv namenode:/home/Datasets/Calendario.csv
+sudo docker cp Datasets/canaldeventa/CanalDeVenta.csv namenode:/home/Datasets/CanalDeVenta.csv
+sudo docker cp Datasets/cliente/Cliente.csv namenode:/home/Datasets/Cliente.csv
+sudo docker cp Datasets/compra/Compra.csv namenode:/home/Datasets/Compra.csv
+sudo docker cp Datasets/empleado/Empleado.csv namenode:/home/Datasets/Empleado.csv
+sudo docker cp Datasets/gasto/Gasto.csv namenode:/home/Datasets/Gasto.csv
+sudo docker cp Datasets/producto/Producto.csv namenode:/home/Datasets/Producto.csv
+sudo docker cp Datasets/proveedor/Proveedor.csv namenode:/home/Datasets/Proveedor.csv
+sudo docker cp Datasets/sucursal/Sucursal.csv namenode:/home/Datasets/Sucursal.csv
+sudo docker cp Datasets/tipodegasto/TiposDeGasto.csv namenode:/home/Datasets/TiposDeGasto.csv
+sudo docker cp Datasets/venta/Venta.csv namenode:/home/Datasets/Venta.csv
 
 sudo docker cp Datasets/data_nvo/Cliente.csv namenode:/home/Datasets/data_nvo/Cliente.csv
 sudo docker cp Datasets/data_nvo/Empleado.csv namenode:/home/Datasets/data_nvo/Empleado.csv
 sudo docker cp Datasets/data_nvo/Producto.csv namenode:/home/Datasets/data_nvo/Producto.csv
 
-sudo docker cp Datasets/.csv namenode:/home/Datasets
+sudo docker cp Datasets/. namenode:/home/Datasets
 
 # Ejecutar comandos dentro del contenedor "namenode"
 sudo docker exec -it namenode bash -c " 
