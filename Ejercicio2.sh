f642efef53105e8e6f473e163725c9f944de4a17
HLussiatti <hernanlussiatti@gmail.com>
2024-09-07 18:10:43 -0300

    07/09/2024 - Ejercicio Integrador

--- Ejercicio2.sh
+++ Ejercicio2.sh
@@ -9,9 +9,5 @@ cd ..
 
 # Copio el hql al contenedor
 sudo docker cp Paso02.hql hive-server:/opt/hive/scripts/Paso02.hql
-# Ejecuto el contenedor
-sudo docker exec -it hive-server bash
 # Ejecuto el hql desde el contenedor
-hive -f /opt/hive/scripts/Paso02.hql
-# Salgo del contenedor
-sudo docker exec -it hive-server bash -c "exit"
+sudo docker exec -it hive-server bash -c "hive -f /opt/hive/scripts/Paso02.hql"
