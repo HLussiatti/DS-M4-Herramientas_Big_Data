e252b0c499c03a6db55bfec9d78fffbceeb55ca6
HLussiatti <hernanlussiatti@gmail.com>
2024-09-08 10:48:59 -0300

    08/09/2024 - Ejercicio Integrador

--- Ejercicio5_2.sh
+++ Ejercicio5_2.sh
@@ -11,8 +11,11 @@ sudo docker cp Datasets/iris.json mongodb:/data/iris.json
 
 # Ejecutar un bash shell dentro del contenedor MongoDB
 sudo docker exec -it mongodb bash -c "
-# Eliminar la base de datos si existe
-mongo --eval 'db.getSiblingDB(\"dataprueba\").dropDatabase()' &&
+
+# Comprobar y eliminar la base de datos si existe
+mongosh --eval '
+db.getSiblingDB(\"dataprueba\").dropDatabase();
+'
 
 # Importar datos en MongoDB
 mongoimport /data/iris.csv --type csv --headerline -d dataprueba -c iris_csv &&
@@ -33,20 +36,27 @@ mongoexport --db dataprueba --collection iris_json --fields sepal_length,sepal_w
 
 # Primero creo el directorio para poder ubicar el .jar
 sudo docker exec -it namenode bash -c "
+# Borro si ya existe
+rm -rf /tmp/udfs/
 cd tmp
 mkdir udfs
 exit
 "
-
 # Copio los JARs que están en la carpeta Mongo a Hive Server
 sudo docker cp Mongo/mongo-hadoop-hive-2.0.2.jar hive-server:/opt/hive/lib/mongo-hadoop-hive-2.0.2.jar
 sudo docker cp Mongo/mongo-hadoop-core-2.0.2.jar hive-server:/opt/hive/lib/mongo-hadoop-core-2.0.2.jar
 sudo docker cp Mongo/mongo-hadoop-spark-2.0.2.jar hive-server:/opt/hive/lib/mongo-hadoop-spark-2.0.2.jar
 sudo docker cp Mongo/mongo-java-driver-3.12.11.jar hive-server:/opt/hive/lib/mongo-java-driver-3.12.11.jar
-# Esta línea se agrega porque el script tira error de no encontrar el .jar en la ubicación esperada.
+
+sudo docker cp Mongo/mongo-hadoop-hive-2.0.2.jar namenode:/tmp/udfs/mongo-hadoop-hive-2.0.2.jar
+sudo docker cp Mongo/mongo-hadoop-core-2.0.2.jar namenode:/tmp/udfs/mongo-hadoop-core-2.0.2.jar
 sudo docker cp Mongo/mongo-java-driver-3.12.11.jar namenode:/tmp/udfs/mongo-java-driver-3.12.11.jar
+sudo docker cp Mongo/mongo-hadoop-spark-2.0.2.jar namenode:/tmp/udfs/mongo-hadoop-spark-2.0.2.jar
 
-
+# Los muevo dentro del HDFS
+sudo docker exec -it namenode bash -c "
+hdfs dfs -put /tmp/udfs/* /tmp/udfs
+"
 # Copiar archivo HQL y ejecutar Hive
 sudo docker cp iris.hql hive-server:/opt/iris.hql
 sudo docker exec -it hive-server bash -c "
