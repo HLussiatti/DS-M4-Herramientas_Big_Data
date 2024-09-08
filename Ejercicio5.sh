ca104a8cd4c4b431d5d65ad90e66e9c02eedf8d0
HLussiatti <hernanlussiatti@gmail.com>
2024-09-08 09:26:20 -0300

    08/09/2024 - Ejercicio Integrador

--- Ejercicio5.sh
+++ Ejercicio5.sh
@@ -1,32 +1,58 @@
 sudo docker exec -it namenode bash -c "
     #Borro el archivo si existe
-    hdfs dfs rm /hbase/data/personal.csv
+    hdfs dfs -rm /hbase/data/personal.csv
     #Copio el archivo desde el Docker
-    hdfs dfs -put /Datasets//personal.csv /hbase/data/personal.csv
+    hdfs dfs -put /Datasets/personal.csv /hbase/data/personal.csv
     echo 'Archivos .csv copiados a HDFS en /hbase/data//'
 "
 
-# Ejecutar comandos HBase en el contenedor hbase-master
+# Para volver a correr y modificar las tablas, las borro primero si existen.
+sudo docker exec -it hbase-master bash -c "
+    echo 'Verifico si existe personal y album en HBase...'
+    hbase shell <<EOF
+    list | grep 'personal'
+    if [ \$? -eq 0 ]; then
+        echo 'Eliminando personal...'
+        disable 'personal'
+        drop 'personal'
+    else
+        echo 'Table personal does not exist.'
+    fi
+
+    list | grep 'album'
+    if [ \$? -eq 0 ]; then
+        echo 'Eliminando album...'
+        disable 'album'
+        drop 'album'
+    else
+        echo 'Table album does not exist.'
+    fi
+EOF
+"
+
+
+
+# Ejecutar comandos HBase en el contenedor hbase-master - Importante la indentación dentro del Shell
+
 # Creo una tabla personal (ID 1 a 4)
 sudo docker exec -it hbase-master bash -c "
     echo 'Creando la tabla persona en HBase y se coloca información manualmente, resultado: personal 4...'
     hbase shell <<EOF
-    create 'personal','personal_data'
-    list 'personal'
-    put 'personal',1,'personal_data:name','Juan'
-    put 'personal',1,'personal_data:city','Córdoba'
-    put 'personal',1,'personal_data:age','25'
-    put 'personal',2,'personal_data:name','Franco'
-    put 'personal',2,'personal_data:city','Lima'
-    put 'personal',2,'personal_data:age','32'
-    put 'personal',3,'personal_data:name','Ivan'
-    put 'personal',3,'personal_data:age','34'
-    put 'personal',4,'personal_data:name','Eliecer'
-    put 'personal',4,'personal_data:city','Caracas'
-    get 'personal','4'
-    EOF
+create 'personal','personal_data'
+put 'personal',1,'personal_data:name','Juan'
+put 'personal',1,'personal_data:city','Córdoba'
+put 'personal',1,'personal_data:age','25'
+put 'personal',2,'personal_data:name','Franco'
+put 'personal',2,'personal_data:city','Lima'
+put 'personal',2,'personal_data:age','32'
+put 'personal',3,'personal_data:name','Ivan'
+put 'personal',3,'personal_data:age','34'
+put 'personal',4,'personal_data:name','Eliecer'
+put 'personal',4,'personal_data:city','Caracas'
+list 'personal'
+get 'personal','4'
+EOF
 "
-
 
 # Ejecuto el comando ImportTsv en HBase para importar el csv en personal (ID 5 a 8)
 sudo docker exec -it hbase-master bash -c "
@@ -34,16 +60,25 @@ sudo docker exec -it hbase-master bash -c "
     hbase org.apache.hadoop.hbase.mapreduce.ImportTsv -Dimporttsv.separator=',' -Dimporttsv.columns=HBASE_ROW_KEY,personal_data:name,personal_data:city,personal_data:age personal hdfs://namenode:9000/hbase/data/personal.csv
 "
 
-# Crear otra tabla de albums de musica
+#Verifico la carga de los datos del csv
+sudo docker exec -it hbase-master bash -c "
+    echo 'Creando la tabla persona en HBase y se coloca información manualmente, resultado: personal 8...'
+    hbase shell <<EOF
+list 'personal'
+get 'personal','8'
+EOF
+"
+
+# Crear otra tabla de albums de musica - Importante la indentación dentro del Shell
 sudo docker exec -it hbase-master bash -c "
     echo 'Creando la tabla de albums...'
     hbase shell <<EOF
-    create 'album','label','image'
-    put 'album','label1','label:size','10'
-    put 'album','label1','label:color','255:255:255'
-    put 'album','label1','label:text','Family album'
-    put 'album','label1','image:name','holiday'
-    put 'album','label1','image:source','/tmp/pic1.jpg'
-    get 'album','label1'
-    EOF
+create 'album','label','image'
+put 'album','label1','label:size','10'
+put 'album','label1','label:color','255:255:255'
+put 'album','label1','label:text','Family album'
+put 'album','label1','image:name','holiday'
+put 'album','label1','image:source','/tmp/pic1.jpg'
+get 'album','label1'
+EOF
 "
