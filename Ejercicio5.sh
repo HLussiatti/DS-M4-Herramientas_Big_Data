2e282cb4fc911c160187986b09ee9a86b91117d6
HLussiatti <hernanlussiatti@gmail.com>
2024-09-08 09:14:42 -0300

    08/09/2024 - Ejercicio Integrador

--- Ejercicio5.sh
+++ Ejercicio5.sh
@@ -1,8 +1,16 @@
 sudo docker exec -it namenode bash -c "
-	hdfs dfs -put /home/Datasets/personal.csv /hbase/data/personal.csv 
-    "
+    #Borro el archivo si existe
+    hdfs dfs rm /hbase/data/personal.csv
+    #Copio el archivo desde el Docker
+    hdfs dfs -put /Datasets//personal.csv /hbase/data/personal.csv
+    echo 'Archivos .csv copiados a HDFS en /hbase/data//'
+"
 
-sudo docker exec -it hbase-master hbase bash -c " 
+# Ejecutar comandos HBase en el contenedor hbase-master
+# Creo una tabla personal (ID 1 a 4)
+sudo docker exec -it hbase-master bash -c "
+    echo 'Creando la tabla persona en HBase y se coloca información manualmente, resultado: personal 4...'
+    hbase shell <<EOF
     create 'personal','personal_data'
     list 'personal'
     put 'personal',1,'personal_data:name','Juan'
@@ -16,11 +24,20 @@ sudo docker exec -it hbase-master hbase bash -c "
     put 'personal',4,'personal_data:name','Eliecer'
     put 'personal',4,'personal_data:city','Caracas'
     get 'personal','4'
+    EOF
+"
 
+
+# Ejecuto el comando ImportTsv en HBase para importar el csv en personal (ID 5 a 8)
+sudo docker exec -it hbase-master bash -c "
+    echo 'Importo perosnal.csv en HBase...'
     hbase org.apache.hadoop.hbase.mapreduce.ImportTsv -Dimporttsv.separator=',' -Dimporttsv.columns=HBASE_ROW_KEY,personal_data:name,personal_data:city,personal_data:age personal hdfs://namenode:9000/hbase/data/personal.csv
-    
-    scan 'personal'
-    
+"
+
+# Crear otra tabla de albums de musica
+sudo docker exec -it hbase-master bash -c "
+    echo 'Creando la tabla de albums...'
+    hbase shell <<EOF
     create 'album','label','image'
     put 'album','label1','label:size','10'
     put 'album','label1','label:color','255:255:255'
@@ -28,4 +45,5 @@ sudo docker exec -it hbase-master hbase bash -c "
     put 'album','label1','image:name','holiday'
     put 'album','label1','image:source','/tmp/pic1.jpg'
     get 'album','label1'
-    "
+    EOF
+"
